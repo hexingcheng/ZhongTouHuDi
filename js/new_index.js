7866eced78fe4397a7eef43da37b837f8b83eb80
hexingcheng <946834832@qq.com>
2015-07-29 15:40:16 +0800

    hxc change

--- js/new_index.js
+++ js/new_index.js
@@ -31,29 +31,27 @@ window.addEventListener("swiperight", function() {
 });
 
 mui.plusReady(function() {
-		main = plus.webview.currentWebview().opener();
-		document.getElementById('close').addEventListener('tap', function(e) {
-				ifloginCommon(function() {
-					mui.ajax(BASEURL + 'auth/out', {
-						type: "get",
-						success: function(data) {
-							console.log('success')
-							plus.storage.removeItem('token')
-						},
-						error: function(xhr, type) {
-							console.log(type);
-							errorhandle(type)
-						}
-					})
-				})
-				return false;
-			})
+//		main = plus.webview.currentWebview().opener();
+//		document.getElementById('close').addEventListener('tap', function(e) {
+//				ifloginCommon(function() {
+//					mui.ajax(BASEURL + 'auth/out', {
+//						type: "get",
+//						success: function(data) {
+//							console.log('success')
+//							plus.storage.removeItem('token')
+//						},
+//						error: function(xhr, type) {
+//							console.log(type);
+//							errorhandle(type)
+//						}
+//					})
+//				})
+//				return false;
+//			})
 			//处理点击事件，跳转页面
 		mui('.mui-table-view-cell').on('tap', 'a', function() {
-			if (this.id && this.id != 'close') {
 				var url = this.getAttribute('data-src');
 				openWindow(url);
-			}
 		})
 	})
 	//处理返回键
