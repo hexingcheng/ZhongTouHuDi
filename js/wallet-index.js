c5fb519d1f1b98dd3b9897008ba49ed9a2666053
lixinqi <250626072@qq.com>
2015-08-06 20:45:50 +0800

    divide wallet-index.js form lee

---
+++ js/wallet-index.js
@@ -0,0 +1,49 @@
+mui.init({
+	swipeBack　: true
+});
+mui.ready(function() {
+
+	var money = document.getElementById("current-money");
+	var point = document.getElementById("gold-point");
+	var coupon = document.getElementById("coupon");
+	var showlist = document.getElementById("show-recodes");
+	var url = "http://202.202.43.107:8080/api/wallet/info";
+	var listobj = null;
+	// 发送给ajax获取余额信息显示
+	console.log("nihao")
+	mui.ajax(url, {
+		type: "post",
+		success: function(data) {
+			if (data.ret == 0) {
+				money.innerHTML = data.res.money;
+				point.innerHTML = data.res.point;
+				coupon.innerHTML = data.res.coupon;
+				var obj = {
+					list: data.res.logList
+				}
+				var html = template("recode", obj);
+				showlist.innerHTML = html;
+				// 获取当前数据储存在storage中，避免再次进行ajax请求发送
+				listobj = JSON.stringify(data.res.logList);
+			}
+		},
+		error: function(xhr, type) {
+			console.log(type)
+		}
+	})
+
+	// 点击链接请求
+	mui("#show-info").on("tap", "a", function(eve) {
+		eve.preventDefault();
+		var href = this.getAttribute("data-src");
+		var obj = {
+			money: money.innerHTML,
+			point: point.innerHTML,
+			coupon: coupon.innerHTML,
+			list: listobj
+		}
+		plus.storage.setItem("recode", JSON.stringify(obj));
+		openWindow(href)
+	})
+
+})
