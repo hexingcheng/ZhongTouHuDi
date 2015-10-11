e5c5185481cc22a910b8a79c6b0408113af482bc
smart_he <946834832@qq.com>
2015-10-11 14:18:20 +0800

    修改

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 var BASEURL = "http://202.202.43.107:8080/api/";
 var BASEIMGURL = "http://202.202.43.107:8080";
 //		var BASEURL = "http://192.168.0.100:8080/api/"
-
+var google = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDbxX3v0OCgYabsgQcUjrQlF4rGhlL7-2Y&v=3.exp&language=zh-CN&signed_in=true&libraries=places'
 function getcamera(successcb, errorcb, option) {
 	var cmr = plus.camera.getCamera();
 	cmr.captureImage(successcb, errorcb)
@@ -24,37 +24,37 @@ function iflogin(cb) {
 }
 
 function ifloginCommon(cb) {
-	if (getstorage('token')) {
-		cb();
-	} else {
-		var i = plus.webview.getLaunchWebview();
-		i.setStyle({
-			left: '0',
-			mask: 'none'
-		})
-		openWindow('./page/logupin/login.html')
+		if (getstorage('token')) {
+			cb();
+		} else {
+			var i = plus.webview.getLaunchWebview();
+			i.setStyle({
+				left: '0',
+				mask: 'none'
+			})
+			openWindow('./page/logupin/login.html')
+		}
 	}
-}
-//  ajax通用函数参数说明 options:基本配置参数，url、type（默认 post 可不填）、data 必填,successcb成功回调  必填 参数为请求返回data对象，
-//errorcb：必填 失败回调 参数 xhr,type,nonetworkcb 无网络回调 选填
-//eg:
-//myAjax({url:"auth/regDynCode",data:{phone:11010101011}},function(data){
-//	console.log(data.code);
-//},function(xhr, type){
-//	console.log(xhr.status);
-//},function(){
-//	alert('没网呀')
-//})
+	//  ajax通用函数参数说明 options:基本配置参数，url、type（默认 post 可不填）、data 必填,successcb成功回调  必填 参数为请求返回data对象，
+	//errorcb：必填 失败回调 参数 xhr,type,nonetworkcb 无网络回调 选填
+	//eg:
+	//myAjax({url:"auth/regDynCode",data:{phone:11010101011}},function(data){
+	//	console.log(data.code);
+	//},function(xhr, type){
+	//	console.log(xhr.status);
+	//},function(){
+	//	alert('没网呀')
+	//})
 
 
 
-function myAjax(options, successcb, errorcb,nonetworkcb) {
+function myAjax(options, successcb, errorcb, nonetworkcb) {
 
 	var net = plus.networkinfo.getCurrentType();
 	if (net != 0 && net != 1) {
 		innerAjax(options, successcb, errorcb)
 	} else {
-		if(nonetworkcb){
+		if (nonetworkcb) {
 			nonetworkcb()
 		} else {
 			mui.toast('未连接网络,请链接网络');
@@ -62,43 +62,45 @@ function myAjax(options, successcb, errorcb,nonetworkcb) {
 	}
 }
 
-function innerAjax(options,successcb,errorcb) {
+function innerAjax(options, successcb, errorcb) {
 	var op = {
-		type:'post',
-		url:'',
-		data:{},
-		wait:false
+		type: 'post',
+		url: '',
+		data: {},
+		wait: false
 	};
-	copyobj(options,op);
-	if(op.wait){
-		plus.nativeUI.showWaiting('请求中',{background:"#d1d1d1"})
+	copyobj(options, op);
+	if (op.wait) {
+		plus.nativeUI.showWaiting('请求中', {
+			background: "#d1d1d1"
+		})
 	}
-//	openwindowloading(function(){
-		mui.ajax(BASEURL + op.url, {
+	//	openwindowloading(function(){
+	mui.ajax(BASEURL + op.url, {
 			type: op.type,
 			data: op.data,
 			success: function(data) {
-				if(op.wait){
+				if (op.wait) {
 					plus.nativeUI.closeWaiting();
 				}
 				if (data.ret == 1) {
 					successcb(data);
 				} else if (data.ret == -101) {
-					if(getstorage('token')){
+					if (getstorage('token')) {
 						var token = getstorage('token');
-						mui.ajax(BASEURL+'auth/activate',{
-							type:'post',
-							data:{
-								token:token
+						mui.ajax(BASEURL + 'auth/activate', {
+							type: 'post',
+							data: {
+								token: token
 							},
-							success:function(data){
-								if(data.ret==1){
-									innerAjax(options,successcb,errorcb);
-								}else if(data.ret==-1){
+							success: function(data) {
+								if (data.ret == 1) {
+									innerAjax(options, successcb, errorcb);
+								} else if (data.ret == -1) {
 									mui.toast('身份校验异常,请重新登录');
-									mui.ajax(BASEURL+'auth/out',{
-										type:'get',
-										success:function(){
+									mui.ajax(BASEURL + 'auth/out', {
+										type: 'get',
+										success: function() {
 											openWindow('./page/logupin/login.html')
 										}
 									})
@@ -108,76 +110,79 @@ function innerAjax(options,successcb,errorcb) {
 					} else {
 						openWindow('./page/logupin/login.html');
 					}
-				} else{
-					if(op.wait){
+				} else {
+					if (op.wait) {
 						plus.nativeUI.closeWaiting()
 					}
 					successcb(data);
 				}
 			},
-			error: function(xhr,type){
-				if(op.wait){
+			error: function(xhr, type) {
+				if (op.wait) {
 					plus.nativeUI.closeWaiting();
 				}
-				errorcb(xhr,type)
+				errorcb(xhr, type)
 			}
 		})
-//		removeloading()
-//	});
+		//		removeloading()
+		//	});
 }
-function copyobj(from,to){
-	for(var i in from){
-		if(typeof from[i]=='object'){
-			copyobj(from[i],to[i])
-		}else{
-			to[i]=from[i];
+
+function copyobj(from, to) {
+	for (var i in from) {
+		if (typeof from[i] == 'object') {
+			copyobj(from[i], to[i])
+		} else {
+			to[i] = from[i];
 		}
 	}
 }
+
 function openWindow(url, param, ani, time) {
-//		openwindowloading(function(){
-			var snum, id;
-			var animationType = ani || 'slide-in-right';
-			var animationTime = time || 300;
-			param = param || {};
-			var pnum = url.indexOf('.html');
-			if (url.indexOf('page') != -1) {
-				snum = url.indexOf('e/') + 1;
-			} else {
-				snum = url.indexOf('/');
-			}
-			id = url.substring(snum + 1, pnum)
-			if (window.plus) {
-				mui.openWindow({
-					id: id,
-					url: url,
-					extras: param,
-					show: {
-						autoShow: true,
-						aniShow: animationType,
-						duration: animationTime
-					},
-					waiting: {
-						autoShow: false,
-						title: '正在加载...',
-						options: {
-							background: '#d1d1d1'
-						}
+		//		openwindowloading(function(){
+		var snum, id;
+		var animationType = ani || 'slide-in-right';
+		var animationTime = time || 300;
+		param = param || {};
+		var pnum = url.indexOf('.html');
+		if (url.indexOf('page') != -1) {
+			snum = url.indexOf('e/') + 1;
+		} else {
+			snum = url.indexOf('/');
+		}
+		id = url.substring(snum + 1, pnum)
+		if (window.plus) {
+			mui.openWindow({
+				id: id,
+				url: url,
+				extras: param,
+				show: {
+					autoShow: true,
+					aniShow: animationType,
+					duration: animationTime
+				},
+				waiting: {
+					autoShow: false,
+					title: '正在加载...',
+					options: {
+						background: '#d1d1d1'
 					}
-				})
-			} else {
-				alert('system is not ready')
-			}
-//			removeloading()
-//		})
+				}
+			})
+		} else {
+			alert('system is not ready')
+		}
+		//			removeloading()
+		//		})
 	}
 	// for test
-function openwindowloading(callback){
-//	var otherload = document.getElementById("loading-mask");
-//	var show = otherload ? getComputedStyle(otherload, 0)["display"] : false;
-//	console.log(show);
-	
-	if(!document.getElementById("openwindowloading")){
+
+function openwindowloading(callback) {
+	//	var otherload = document.getElementById("loading-mask");
+	//	var show = otherload ? getComputedStyle(otherload, 0)["display"] : false;
+	//	console.log(show);
+
+	if (!document.getElementById("openwindowloading")) {
 		var div = document.createElement('div');
 		div.id = 'openwindowloading'
 		var loading = document.createElement('div');
@@ -202,18 +207,20 @@ function openwindowloading(callback){
 		div.style.right = '0px';
 		div.style.bottom = '0px';
 		div.style.zIndex = 1000;
-		img.onload = function(){
+		img.onload = function() {
 			document.getElementsByTagName('body')[0].appendChild(div);
 			callback();
 		}
 	}
 }
-function removeloading(){
+
+function removeloading() {
 	var loading = document.getElementById('openwindowloading');
-	setTimeout(function(){
+	setTimeout(function() {
 		document.getElementsByTagName('body')[0].removeChild(loading)
-	},500)
+	}, 500)
 }
+
 function openNewWindow(url, param, ani, time) {
 	var snum, id;
 	var animationType = ani || 'slide-in-right';
@@ -278,6 +285,7 @@ function errorhandle(type) {
 	}
 	return;
 }
+
 function getuserbasicinfo() {
 	var obj = {};
 	mui.plusReady(function() {
@@ -314,7 +322,7 @@ function formRegTest() {
 }
 
 
-function closeMask(){
+function closeMask() {
 	// 300毫秒之后清除等待框
 	var lmask = document.getElementById("loading-mask");
 	var lbox = document.getElementById("loading-box");
@@ -330,69 +338,101 @@ function closeMask(){
 		bcl.remove("fade-out");
 	}, false)
 }
-function getcurrenttime(){
+
+function getcurrenttime() {
 	var d = new Date();
-	var m = d.getMonth()-0+1;
-	var nm = m<10?('0'+m):m;
+	var m = d.getMonth() - 0 + 1;
+	var nm = m < 10 ? ('0' + m) : m;
 	var da = d.getDate();
-	var nd = da<10?('0'+d):da;
+	var nd = da < 10 ? ('0' + d) : da;
 	var h = d.getHours();
-	var nh = h<10?('0'+h):h;
+	var nh = h < 10 ? ('0' + h) : h;
 	var mi = d.getMinutes();
-	var nmi = mi<10?('0'+mi):mi;
+	var nmi = mi < 10 ? ('0' + mi) : mi;
 	var s = d.getSeconds();
-	var ns = s<10?('0'+s):s;
-	return d.getFullYear()+"-"+nm+"-"+nd+" "+nh+":"+mi+":"+ns;
+	var ns = s < 10 ? ('0' + s) : s;
+	return d.getFullYear() + "-" + nm + "-" + nd + " " + nh + ":" + mi + ":" + ns;
 }
 
 // 解决部分手机number类型的时候不出现placeholder属性值
-function fixedNum(selector){
-	$(selector).on("focus", function(){
+function fixedNum(selector) {
+	$(selector).on("focus", function() {
 		$(this).attr("type", "number");
 	})
-	$(selector).on("blur", function(){
+	$(selector).on("blur", function() {
 		$(this).attr("type", "text");
 	})
 }
-function getlatlng(success){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(pos){
+
+function getlatlng(success) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(pos) {
 			//存储经纬度信息
-			var lat = pos.coords.latitude+"";
-			var lng = pos.coords.longitude+"";
-			plus.storage.setItem('latitude',lat)
-			plus.storage.setItem('longitude',lng)
-			console.log('获取当前位置成功lat:'+plus.storage.getItem('latitude')+',lng:'+plus.storage.getItem('longitude'));
-			success&&success(pos);
-		},function(e){
-			mui.toast('获取当前位置信息失败')
-		},{ 
-			enableHighAcuracy: true
+			var lat = pos.coords.latitude + "";
+			var lng = pos.coords.longitude + "";
+			plus.storage.setItem('latitude', lat)
+			plus.storage.setItem('longitude', lng)
+			console.log('获取当前位置成功lat:' + plus.storage.getItem('latitude') + ',lng:' + plus.storage.getItem('longitude'));
+//			var geocoder = new google.maps.Geocoder();
+//			var center = {
+//				lat: pos.coords.latitude,
+//				lng: pos.coords.longitude
+//			}
+//			geocoder.geocode({
+//				'location': center
+//			}, function(results, status) {
+//				if (status === google.maps.GeocoderStatus.OK) {
+//					success && success(pos);
+//					if (results[1]) {
+//						var currentaddr = results[1].formatted_address
+//						plus.storage.setItem('currentaddr', currentaddr);
+//						console.log(plus.storage.getItem('currentaddr'))
+//						success && success();
+//					} else {
+//						success && success();
+//						alert('No results found');
+//					}
+//				} else {
+//					success && success();
+//					window.alert('Geocoder failed due to: ' + status);
+//				}
+//			});
+			plus.storage.setItem('currentaddr', '中国重庆市南岸区');
+			console.log(plus.storage.getItem('currentaddr'))
+			success && success();
+		}, function(e) {
+			success && success();
+			mui.toast('获取当前位置信息失败');
+		}, {
+			enableHighAcuracy: true,
+			timeout: 15000
 		})
-	}else{
+	} else {  
+		success && success();
 		mui.toast('您的手机不支持定位功能')
 	}
 }
-function closeweb(webarr){
-	var len = webarr.length;
-	for(var i = 0, web = plus.webview.getWebviewById(webarr[i]);i<len;i++){
-		if(web){
-			web.close()
+
+function closeweb(webarr) {
+		var len = webarr.length;
+		for (var i = 0, web = plus.webview.getWebviewById(webarr[i]); i < len; i++) {
+			if (web) {
+				web.close()
+			}
 		}
 	}
-}
-//function getmsgs() {
-//	myAjax({
-//		url: 'message/getMsg'
-//	}, function(data) {
-//		if (data.ret == 1) {
-//			alert(data.res.msgList.length)
-//			if(data.res.msgList.length){
-//				alert('news')
-//				$('#msgtips').attr('src','./img/tips.png')
-//			}
-//		}
-//	}, function(xhr, type) {
-//		mui.toast(xhr.status + ":" + type)
-//	})
-//}
+	//function getmsgs() {
+	//	myAjax({
+	//		url: 'message/getMsg'
+	//	}, function(data) {
+	//		if (data.ret == 1) {
+	//			alert(data.res.msgList.length)
+	//			if(data.res.msgList.length){
+	//				alert('news')
+	//				$('#msgtips').attr('src','./img/tips.png')
+	//			}
+	//		}
+	//	}, function(xhr, type) {
+	//		mui.toast(xhr.status + ":" + type)
+	//	})
+	//}
